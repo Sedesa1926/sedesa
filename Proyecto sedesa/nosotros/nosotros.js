24412837a4b9f8abf650052b12d1c81d86a2cbff
Sedesa1926 <91704596+Sedesa1926@users.noreply.github.com>
2021-09-30 18:26:44 -0500

    Add files via upload

---
+++ Proyecto sedesa/nosotros/nosotros.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal();
+
+
+sr.reveal('.container-all',{
+    duration: 1000 
+    });
+    sr.reveal('.container',{
+    duration:1500,
+    origin:'right',
+    });
+    sr.reveal('.container-s',{
+        duration:2000,
+        origin:'bottom',
+        distance:'100px'
+    });
+    
+    sr.reveal('.container-third',{
+        duration:2000,
+        origin:'left',
+        distance:'100px'
+    });
+    
+    sr.reveal('.first-text',{
+        duration:2000,
+        origin:'left',
+        distance:'100px'
+    });
+    
+    sr.reveal('.second-text',{
+        duration:2000,
+        origin:'left',
+        distance:'100px'
+    });
---
+++ Proyecto sedesa/principal.js
@@ -0,0 +1,198 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.container-all',{
+duration: 1000 
+});
+sr.reveal('.container',{
+duration:1500,
+origin:'right',
+});
+
+sr.reveal('.img2',{
+    duration:1500,
+    origin:'right',
+});
+sr.reveal('.container-second',{
+    duration:1500,
+    origin:'bottom',
+    distance:'100px'
+
+});
+
+sr.reveal('.container-a',{
+    duration:1500,
+    origin:'bottom',
+    distance:'100px'
+
+});
+
+sr.reveal('.container-b',{
+    duration:1500,
+    origin:'left',
+    distance:'100px'
+
+});
+
+sr.reveal('.container-c',{
+    duration:1500,
+    origin:'right',
+    
+
+});
+
+sr.reveal('.container-e',{
+    duration:1500,
+    origin:'bottom',
+    distance:'100px'
+
+});
+sr.reveal('.container-f',{
+    duration:1500,
+    origin:'left',
+    distance:'100px'
+
+});
+sr.reveal('.container-g',{
+    duration:1500,
+    origin:'bottom',
+distance:'10px'
+
+});
+
+sr.reveal('.container-h',{
+    duration:1500,
+    origin:'bottom',
+    distance:'50px'
+    
+
+});
+
+sr.reveal('.container1',{
+    duration:1500,
+    origin:'bottom',
+    distance:'100px'
+
+});
+
+
+sr.reveal('.container-k',{
+    duration:1500,
+    origin:'bottom',
+    distance:'100px'
+
+});
+
+sr.reveal('.container-l',{
+    duration:1500,
+    origin:'left',
+distance:'100px'
+});
+sr.reveal('.container-m',{
+    duration:1500,
+    origin:'left',
+distance:'100px'
+
+});
+sr.reveal('.container-n',{
+    duration:1500,
+    
+    origin:'left',
+distance:'100px'
+
+});
+sr.reveal('.container-o',{
+    duration:1500,
+
+    origin:'left',
+distance:'100px'
+});
+
+sr.reveal('.servicios1',{
+    duration:1500,
+    origin:'bottom',
+    distance:'100px'
+
+});
+
+sr.reveal('.first',{
+    duration:1500,
+    origin:'right',
+    distance:''
+
+});
+
+sr.reveal('.ch',{
+    duration:1500,
+    origin:'right',
+    distance:''
+
+});
+
+sr.reveal('.cb',{
+    duration:1500,
+    origin:'right',
+    distance:''
+
+});
+
+sr.reveal('.cc',{
+    duration:1500,
+    origin:'right',
+    distance:''
+
+});
+
+sr.reveal('.cd',{
+    duration:1500,
+    origin:'right',
+    distance:''
+
+});
+
+sr.reveal('.cf',{
+    duration:1500,
+    origin:'right',
+    distance:''
+
+});
+sr.reveal('.second',{
+    duration:1500,
+    origin:'left',
+    distance:'100px'
+
+});
+
+sr.reveal('.ce',{
+    duration:1500,
+    origin:'left',
+    distance:'100px'
+
+});
+
+sr.reveal('.cg',{
+    duration:1500,
+    origin:'left',
+    distance:'100px'
+
+});
+
+sr.reveal('.ca',{
+    duration:1500,
+    origin:'left',
+    distance:'100px'
+
+});
+
+sr.reveal('.ci',{
+    duration:1500,
+    origin:'left',
+    distance:'100px'
+
+});
+
+sr.reveal('.cj',{
+    duration:1500,
+    origin:'left',
+    distance:'100px'
+
+});
